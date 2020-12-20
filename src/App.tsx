c16195dab67df0aaaec514a09dc0ad0ae070c0d6
fed-gren <deveb22@gmail.com>
2020-12-20 20:53:36 +0900

    feat: Styled Button 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  color: black;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello world!
-      </header>
-    </div>
+    <Button>버튼입니다.</Button>
   );
 }
 
